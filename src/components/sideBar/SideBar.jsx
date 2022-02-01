0a0515376bccfead2a434f09de2db71caaa83ba4
Anton Sedchenko <anton.sedchenko@gmail.com>
2022-02-01 19:00:11 +0200

    The sidebar hide and open on burger btn click

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './SideBar.css';
 import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 import { logOut } from '../../store/authReducer';
-import {useDispatch, useSelector} from 'react-redux';
+import { changeSideBarState } from '../../store/sideBarReducer';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import MessageIcon from '@mui/icons-material/Message';
@@ -19,7 +20,16 @@ const SideBarWrapper = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    padding-bottom: 41px;
+`;
+
+const SideBarLinkContent = styled.div`
+    display: flex;
+    justify-content: ${({isSideBarWide}) => (isSideBarWide ? 'flex-start' : 'center')};
+    align-items: center;
+    padding-left: ${({isSideBarWide}) => (isSideBarWide ? '22px' : '')};
+    height: 60px;
+    font-size: 16px;
+    text-decoration: none;
 `;
 
 const SideBar = () => {
@@ -57,48 +67,56 @@ const SideBar = () => {
 
     return (
         <SideBarWrapper isSideBarWide={isSideBarWide}>
-            <div className="sidebar__page-links-container">
-                <div className="sidebar__page-link-wrapper">
-                    <Link to="/home" className={`sidebar__page-link ${homePageLinkClass}`}
-                        onClick={() => {
-                            setHomePageLinkClass('sidebar__page-link--active');
-                            setChatPageLinkClass('');
-                            setSettingsPageLinkClass('');
-                        }}
-                    >
-                        <HomeIcon className="sidebar__page-icon" />
-                    </Link>
-                </div>
-                <div className="sidebar__page-link-wrapper">
-                    <Link to="/chat" className={`sidebar__page-link ${chatPageLinkClass}`}
-                        onClick={() => {
-                            setHomePageLinkClass('');
-                            setChatPageLinkClass('sidebar__page-link--active');
-                            setSettingsPageLinkClass('');
-                        }}
-                    >
-                        <MessageIcon className="sidebar__page-icon" />
-                    </Link>
-                </div>
-                <div className="sidebar__page-link-wrapper">
-                    <Link to="/settings" className={`sidebar__page-link ${settingsPageLinkClass}`}
-                        onClick={() => {
-                            setHomePageLinkClass('');
-                            setChatPageLinkClass('');
-                            setSettingsPageLinkClass('sidebar__page-link--active');
-                        }}
-                    >
-                        <SettingsIcon className="sidebar__page-icon" />
-                    </Link>
-                </div>
+            <div className="sidebar__links-container">
+                <Link to="/home" className={`sidebar__page-link ${homePageLinkClass}`}
+                      onClick={() => {
+                          setHomePageLinkClass('sidebar__page-link--active');
+                          setChatPageLinkClass('');
+                          setSettingsPageLinkClass('');
+                      }}
+                >
+                    <SideBarLinkContent isSideBarWide={isSideBarWide}>
+                        <HomeIcon />
+                        {isSideBarWide && <span className="sidebar__page-link-text">Home</span>}
+                    </SideBarLinkContent>
+                </Link>
+                <Link to="/chat" className={`sidebar__page-link ${chatPageLinkClass}`}
+                      onClick={() => {
+                          setHomePageLinkClass('');
+                          setChatPageLinkClass('sidebar__page-link--active');
+                          setSettingsPageLinkClass('');
+                      }}
+                >
+                    <SideBarLinkContent isSideBarWide={isSideBarWide}>
+                        <MessageIcon />
+                        {isSideBarWide && <span className="sidebar__page-link-text">Chat</span>}
+                    </SideBarLinkContent>
+                </Link>
+                <Link to="/settings" className={`sidebar__page-link ${settingsPageLinkClass}`}
+                      onClick={() => {
+                          setHomePageLinkClass('');
+                          setChatPageLinkClass('');
+                          setSettingsPageLinkClass('sidebar__page-link--active');
+                      }}
+                >
+                    <SideBarLinkContent isSideBarWide={isSideBarWide}>
+                        <SettingsIcon />
+                        {isSideBarWide && <span className="sidebar__page-link-text">Settings</span>}
+                    </SideBarLinkContent>
+                </Link>
             </div>
             <div className="sidebar__logout-container">
-                <MeetingRoomIcon
+                <SideBarLinkContent
+                    isSideBarWide={isSideBarWide}
                     onClick={() => {
+                        dispatch(changeSideBarState(!isSideBarWide));
                         dispatch(logOut());
                         navigate('/login');
                     }}
-                />
+                >
+                    <MeetingRoomIcon />
+                    {isSideBarWide && <span className="sidebar__page-link-text">Logout</span>}
+                </SideBarLinkContent>
             </div>
         </SideBarWrapper>
     );
